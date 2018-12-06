5d3e6e3bf8dd7b4d921f31ee11481277fc7de137
Prateek Chaudhary <prateek.chaudhary@idemia.com>
2018-12-06 15:27:47 +0530

    adding aws_ami, main, providers and variables .tf for infra creation on aws and secret key and secret id already present inside the variable file

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_access_key" {
+  default = "AKIAJGBOEZ75F3FKM7IQ"
+}
+
+variable "aws_secret_key" {
+  default = "81hIzzQx5v12NXfxMjFIkgu5nMEPtdukpgPgbTaC"
+}
+
+variable "aws_region" {
+  default = "us-east-2"
+}
